60ccdbe8f91f7f1998281f6df638e2905d6ffae7
RehanG786 <rehanglasswala739@gmail.com>
2025-10-21 20:07:24 +0530

    initial commit

---
+++ js_tutorial.js
@@ -0,0 +1,63 @@
+// Primitive / Value Types
+// String, Number, Boolean, Undefined, NULL, Symbol
+// JS - Dynamic Language
+
+
+// String
+/*let x = "Rehan";
+console.log(x);
+console.log(typeof(x));
+x = 8;
+console.log(x);
+console.log(typeof(x));
+
+// Number
+let y = 1;
+console.log(y);
+console.log(typeof(y));
+
+let z = 1.4;
+console.log(z);
+console.log(typeof(z));
+
+// Boolean
+let p = true;
+console.log(p);
+console.log(typeof(p));
+
+// Undefined
+let w;
+console.log(w);
+w = 0;
+console.log(typeof(w));
+*/
+
+// Reference Types - Objects, Arrays, Functions 
+
+/*let course = {
+    title: "HHLD",
+    description: "Projects",
+    rating: 5
+}
+
+console.log(course);
+console.log(typeof(course));
+
+console.log(course.title);
+console.log(course['description']);*/
+
+let x = "Rehan";
+let y = x;
+
+x = "educosys";
+
+console.log(x);
+console.log(y);
+
+let p = {name: "Rehan"};
+let q = p;
+
+p.name = "educosys";
+
+console.log(p);
+console.log(q);
